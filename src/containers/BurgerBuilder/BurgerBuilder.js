32ae96780482c9f1fa9e93319a0b35a71d0e8d92
Luigi <dev.js.pad@gmail.com>
2019-10-17 17:54:03 +0200

    Add initial files

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Auxiliary from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+    render () {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
